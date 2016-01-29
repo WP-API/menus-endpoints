28f463c1e2a50c533d347002575da5f0092cf8f1
Marcel Schmitz <marcelschmitz@Susanas-MacBook-Air.local>
2016-01-29 14:54:30 +0000

    fixed path issue and instantiated nav menu classes with basic route registration

--- plugin.php
+++ plugin.php
@@ -10,11 +10,25 @@
  */
 
 if ( class_exists( 'WP_REST_Controller' )
-	&& ! class_exists( 'WP_REST_Menus_Controller' ) ) {
-	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-menus-controller.php';
+	&& ! class_exists( 'WP_REST_Nav_Menus_Controller' ) ) {
+	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-nav-menus-controller.php';
 }
 
 if ( class_exists( 'WP_REST_Controller' )
-	&& ! class_exists( 'WP_REST_Menu_Items_Controller' ) ) {
-	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-menu-items-controller.php';
+	&& ! class_exists( 'WP_REST_Nav_Menu_Items_Controller' ) ) {
+	require_once dirname( __FILE__ ) . '/lib/class-wp-rest-nav-menu-items-controller.php';
 }
+
+add_action( 'rest_api_init', 'create_initial_nav_menu_routes', 0 );
+
+
+function create_initial_nav_menu_routes() {
+
+	    $nav_menu_route = new WP_REST_Nav_Menus_Controller();
+        $nav_menu_route->register_routes();
+
+        $nav_menu_item_route = new WP_REST_Nav_Menu_Items_Controller();
+        $nav_menu_item_route->register_routes();
+
+}
+

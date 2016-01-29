abf0b35bfcbb7f8912af4811c133a63c8e5fc6a8
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-29 04:49:22 -0800

    Scaffold some basic code

---
+++ lib/class-wp-rest-menus-controller.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Manage Menus for a WordPress site
+ */
+class WP_REST_Menus_Controller extends WP_REST_Controller {
+
+	public function __construct() {
+		$this->namespace = 'wp/v2';
+		$this->rest_base = 'menus';
+	}
+
+	public function register_routes() {
+		// @todo
+	}
+
+	public function get_items_permissions_check( $request ) {
+
+	}
+
+	public function get_items( $request ) {
+
+	}
+
+	public function get_item_permissions_check( $request ) {
+
+	}
+
+	public function get_item( $request ) {
+
+	}
+
+	public function delete_item_permission_check( $request ) {
+
+	}
+
+	public function delete_item( $request ) {
+
+	}
+
+	public function prepare_item_for_response( $item, $request ) {
+		
+	}
+
+	public function get_item_schema() {
+
+	}
+
+	public function get_collection_params() {
+
+	}
+
+}
---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP REST API - Menu Endpoints
+ * Description: Menu endpoints for the WP REST API
+ * Author: WP REST API Team
+ * Author URI: http://wp-api.org
+ * Version: 0.1.0
+ * Plugin URI: https://github.com/WP-API/wp-api-menus-endpoints
+ * License: GPL2+
+ */
+
+if ( class_exists( 'WP_REST_Controller' )
+	&& ! class_exists( 'WP_REST_Menus_Controller' ) ) {
+	require_once dirname( __FILE__ ) . '/class-wp-rest-menus-controller.php';
+}
+
+if ( class_exists( 'WP_REST_Controller' )
+	&& ! class_exists( 'WP_REST_Menu_Items_Controller' ) ) {
+	require_once dirname( __FILE__ ) . '/class-wp-rest-menu-items-controller.php';
+}

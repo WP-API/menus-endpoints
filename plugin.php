ad4eaaf0d128813d00cc5811c82eac0c3f3dc971
K. Adam White <kadamwhite@users.noreply.github.com>
2019-08-14 14:38:41 -0400

    Remove all widgets code

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 /**
  * Menu endpoints for the WP REST API.
  *
- * Plugin Name: WP REST API - Menus and Widgets Endpoints
- * Description: Menu endpoints for the WP REST API
+ * Plugin Name: WordPress REST API Menus Endpoints
+ * Description: REST endpoints for accessing and interacting with Navigation Menus.
  * Author: WP REST API Team
  * Author URI: http://wp-api.org
  * Version: 0.1.0
- * Plugin URI: https://github.com/WP-API/wp-api-menus-widgets-endpoints
+ * Plugin URI: https://github.com/WP-API/menus-endpoints
  * License: GPL2+
  *
  * @package         REST_API
@@ -15,12 +15,12 @@
  * @license         GPL-2.0+
  */
 
-add_action( 'rest_api_init', 'wp_api_nav_menus_widgets_init_controllers', 0 );
+add_action( 'rest_api_init', 'wp_api_nav_menus_init_controllers', 0 );
 
 /**
  * Bootstrap endpoints.
  */
-function wp_api_nav_menus_widgets_init_controllers() {
+function wp_api_nav_menus_init_controllers() {
 	if ( ! class_exists( 'WP_REST_Controller' ) ) {
 		return;
 	}
@@ -36,36 +36,22 @@ function wp_api_nav_menus_widgets_init_controllers() {
 		require_once dirname( __FILE__ ) . '/lib/class-wp-rest-menu-locations-controller.php';
 	}
 
-	if ( ! class_exists( 'WP_REST_Widgets_Controller' ) ) {
-		require_once dirname( __FILE__ ) . '/lib/class-wp-rest-widgets-controller.php';
-	}
-
 	$nav_menu_location = new WP_REST_Menu_Locations_Controller();
 	$nav_menu_location->register_routes();
-
-	/**
-	 * Get global widget factory.
-	 *
-	 * @type WP_Widget_Factory $wp_widget_factory
-	 */
-	global $wp_widget_factory;
-
-	$widgets_controller = new WP_REST_Widgets_Controller( $wp_widget_factory->widgets );
-	$widgets_controller->register_routes();
 }
 
 
-add_filter( 'register_post_type_args', 'wp_api_nav_menus_widgets_post_type_args', 10, 2 );
+add_filter( 'register_post_type_args', 'wp_api_nav_menus_post_type_args', 10, 2 );
 
 /**
- * Hook in and a post type rest endpoint.
+ * Hook in to the nav menu item post type and enable a post type rest endpoint.
  *
  * @param array  $args Current registered post type args.
- * @param string $post_type name of post type.
+ * @param string $post_type Name of post type.
  *
  * @return array
  */
-function wp_api_nav_menus_widgets_post_type_args( $args, $post_type ) {
+function wp_api_nav_menus_post_type_args( $args, $post_type ) {
 	if ( 'nav_menu_item' === $post_type ) {
 		$args['show_in_rest']          = true;
 		$args['rest_base']             = 'menu-items';
@@ -76,17 +62,17 @@ function wp_api_nav_menus_widgets_post_type_args( $args, $post_type ) {
 }
 
 
-add_filter( 'register_taxonomy_args', 'wp_api_nav_menus_widgets_taxonomy_args', 10, 2 );
+add_filter( 'register_taxonomy_args', 'wp_api_nav_menus_taxonomy_args', 10, 2 );
 
 /**
- * Hook in and a taxonomy rest endpoint.
+ * Hook in to the nav_menu taxonomy and enable a taxonomy rest endpoint.
  *
  * @param array  $args Current registered taxonomy args.
- * @param string $taxonomy name of taxonomy.
+ * @param string $taxonomy Name of taxonomy.
  *
  * @return array
  */
-function wp_api_nav_menus_widgets_taxonomy_args( $args, $taxonomy ) {
+function wp_api_nav_menus_taxonomy_args( $args, $taxonomy ) {
 	if ( 'nav_menu' === $taxonomy ) {
 		$args['show_in_rest']          = true;
 		$args['rest_base']             = 'menus';

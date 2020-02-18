08656b1346682bb03cc75d1de92a97959f818e55
Jonny Harris <spacedmonkey@users.noreply.github.com>
2020-02-18 17:05:10 +0000

    Version 0.5.0

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Description: REST endpoints for accessing and interacting with Navigation Menus.
  * Author: WP REST API Team
  * Author URI: http://wp-api.org
- * Version: 0.4.0
+ * Version: 0.5.0
  * Plugin URI: https://github.com/WP-API/menus-endpoints
  * Requires at least: 4.7
  * Requires PHP: 5.6
